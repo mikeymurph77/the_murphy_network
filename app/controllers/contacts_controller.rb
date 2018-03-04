35269487e689ad4c873c3fad6ca9902e0054fa19
Michael Murphy <mmurphy@jobcase.com>
2018-03-03 19:51:18 -0500

    Logic for rendering and updating existing contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,44 @@
 class ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all.order(:first_name)
+  end
+
+  def show
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+
+    @months = {
+      'January' => '1',
+      'February' => '2',
+      'March' => '3',
+      'April' => '4',
+      'May' => '5',
+      'June' => '6',
+      'July' => '7',
+      'August' => '8',
+      'September' => '9',
+      'October' => '10',
+      'November' => '11',
+      'December' => '12',
+    }
+  end
+
+  def update
+    contact = Contact.find(params[:id])
+
+    if contact.update(contact_params)
+      flash[:success] = 'Contact Saved!'
+      redirect_to contacts_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :email, :phone, :address, :birthday)
+  end
 end
