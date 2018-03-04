64a6431219521f33cacf35a9c1bdac9264aa0524
Michael Murphy <mmurphy@jobcase.com>
2018-03-03 19:51:33 -0500

    Create contact helpers

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,15 @@
 class Contact < ActiveRecord::Base
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def full_address
+    if self.address_line_1.nil?
+      ""
+    elsif self.address_line_2.present?
+      "#{self.address_line_1}, #{self.address_line_2}, #{self.city}, #{self.state} #{self.zip_code}"
+    else
+      "#{self.address_line_1}, #{self.city}, #{self.state} #{self.zip_code}"
+    end
+  end
 end
